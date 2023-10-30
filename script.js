f517e0a7c866d27095cfef6d9f53f79e2a61e012
gui <guigon@alunos.utfpr.edu.br>
2023-10-30 13:05:54 -0300

    feat: add ex04 first program

--- script.js
+++ script.js
@@ -18,3 +18,44 @@ function resolucao03() {
   document.body.style.backgroundColor =
     document.getElementById("colorPicker").value;
 }
+
+function atividade1(num) {
+  var fatorial = num;
+  for (i = num - 1; i > 0; i--) {
+    fatorial = fatorial * i;
+  }
+  return fatorial;
+}
+
+function atividade2(n, mensagem) {
+  stringRetorno = mensagem;
+  for (i = 1; i < n; i++) {
+    stringRetorno += " ";
+    stringRetorno += mensagem;
+  }
+  return stringRetorno;
+}
+
+function atividade3(num1, num2, operacao) {
+  switch (operacao) {
+    case "+":
+      return num1 + num2;
+    case "-":
+      return num1 - num2;
+    case "*":
+      return num1 * num2;
+    case "/":
+      if (num2 == 0) {
+        return null;
+      }
+      return num1 / num2;
+    default:
+      return null;
+  }
+}
+
+function fatorial() {
+  var input = Number(document.getElementById("numeroFatorial").value);
+  var resultado = atividade1(input).toString();
+  document.getElementById("resultadoFatorial").innerHTML = resultado;
+}
