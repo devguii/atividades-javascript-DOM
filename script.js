1f1920a338f4468cda8d79a479a0cb7cac2166e6
gui <guigon@alunos.utfpr.edu.br>
2023-10-30 11:12:16 -0300

    feat: add ex01 logic

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 document.onload(console.log("Iniciou"));
+
+function resolucao01() {
+  var input = document.getElementById("input-ex01").value;
+  var resultado = input.toUpperCase().replace(/[0-9]/g, "");
+  document.getElementById("paragrafo").innerHTML = resultado;
+}
