d67a34db62cb41da8fc91bd7be2af3dbef3ceda6
gui <guigon@alunos.utfpr.edu.br>
2023-11-02 19:50:28 -0300

    feat: fix calculator

--- script.js
+++ script.js
@@ -56,3 +56,26 @@ function invertido() {
   var resultado = atividade5(input).toString();
   document.getElementById("resultadoInvertido").innerHTML = resultado;
 }
+
+function insert(num) {
+  var numero = document.getElementById("resultado").innerHTML;
+  document.getElementById("resultado").innerHTML = numero + num;
+}
+function clean() {
+  document.getElementById("resultado").innerHTML = "";
+}
+function back() {
+  var resultado = document.getElementById("resultado").innerHTML;
+  document.getElementById("resultado").innerHTML = resultado.substring(
+    0,
+    resultado.length - 1
+  );
+}
+function calcular() {
+  var resultado = document.getElementById("resultado").innerHTML;
+  if (resultado) {
+    document.getElementById("resultado").innerHTML = eval(resultado);
+  } else {
+    document.getElementById("resultado").innerHTML = "Nada...";
+  }
+}
